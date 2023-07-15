d9bce35f7a5ff356ece7eb5474f488830e573fff
formaaaol0313 <formaaaol0313@gmail.com>
2023-07-15 14:38:53 -0600

    Cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+apiKey: "AIzaSyCBitZnVOQ28yOxPAXh8wnAliogfuwB4Bo",
+  authDomain: "autic33aaol-36812.firebaseapp.com",
+  projectId: "autic33aaol-36812",
+  storageBucket: "autic33aaol-36812.appspot.com",
+  messagingSenderId: "347016995650",
+  appId: "1:347016995650:web:52987fa3a1ae7a80201edb",
+  measurementId: "G-76XJ6DCHCH"
+    });
